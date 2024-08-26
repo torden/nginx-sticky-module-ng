f2fb5344acb92b8ec597a6eb3dc892b70bb82fe7
Torden <tordengit@gmail.com>
2024-08-26 13:46:51 +0900

    add patches

--- ngx_http_sticky_misc.c
+++ ngx_http_sticky_misc.c
@@ -17,15 +17,22 @@
 #endif
 
 /* - fix for 1.11.2 removes include <openssl/md5.h> in ngx_md5.h */
-#define MD5_CBLOCK  64
-#define MD5_LBLOCK  (MD5_CBLOCK/4)
-#define MD5_DIGEST_LENGTH 16
-#define SHA_CBLOCK 64
+#ifndef MD5_CBLOCK
+    #define MD5_CBLOCK  64
+    #define MD5_LBLOCK  (MD5_CBLOCK/4)
+#endif
+
+#ifndef MD5_DIGEST_LENGTH
+    #define MD5_DIGEST_LENGTH 16
+#endif
+
+#ifndef SHA_CBLOCK
+    #define SHA_CBLOCK 64
+#endif
 #define SHA_DIGEST_LENGTH 20
 
 #ifndef SHA_DIGEST_LENGTH
-#define SHA_CBLOCK 64
-#define SHA_DIGEST_LENGTH 20
+    #define SHA_DIGEST_LENGTH 20
 #endif
 
 // /* - bugfix for compiling on sles11 - needs gcc4.6 or later*/
